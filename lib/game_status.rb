9c5586e0eb8bc7d0abb874d009966bdc6ed01ac3
Allison MacQueen <allimacq@gmail.com>
2020-07-24 15:14:00 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -75,9 +75,9 @@ def winner(board)
   #setting the index of the win into a variable, win
   win = won?(board)
   #all indexes should have the same token for a win, so we're only checking the first
-  if board[win[0]] == "X"
+  if board[win[0]] == "X" && won?(board) != false
     return "X"
-  elsif board[win[0]] == "O"
+  elsif board[win[0]] == "O" && won?(board) != false
     return "O"
   #now returning nil if there is no winner
   elsif won?(board) == false
